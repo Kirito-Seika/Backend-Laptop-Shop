8ac03639a84867b00fa69f226eee87bc91d79c35
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-02-23 08:16:52 +0700

    Java Annotation

--- src/main/java/vn/minhduc/laptopshop/LaptopShopApplication.java
+++ src/main/java/vn/minhduc/laptopshop/LaptopShopApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class LaptopShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopShopApplication.class, args);
+    }
 
 }
