69916b22c30f3331b464b7b169238aea0a7e83f9
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-02-26 07:14:39 +0700

    Render view HTML

--- src/main/java/vn/minhduc/laptopshop/controller/UserController.java
+++ src/main/java/vn/minhduc/laptopshop/controller/UserController.java
@@ -1,19 +1,35 @@
 package vn.minhduc.laptopshop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import vn.minhduc.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHomePage();
+        return "home.html";
     }
 }
+
+//@RestController
+//public class UserController {
+//    private final UserService userService;
+//
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
+//
+//    @GetMapping("/")
+//    public String getHomePage() {
+//        return this.userService.handleHomePage();
+//    }
+//}
