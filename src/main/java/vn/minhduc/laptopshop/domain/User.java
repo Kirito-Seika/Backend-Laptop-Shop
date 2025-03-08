7663b2fb0a288f4e6dbdc225f17b3223ea61a344
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-03-08 18:36:21 +0700

    Shopping Cart Functional Analysis

--- src/main/java/vn/minhduc/laptopshop/domain/User.java
+++ src/main/java/vn/minhduc/laptopshop/domain/User.java
@@ -38,6 +38,9 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Order> orders;
 
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
+
     public long getId() {
         return id;
     }
@@ -110,8 +113,27 @@ public class User {
         this.orders = orders;
     }
 
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", role=" + role +
+                ", orders=" + orders +
+                ", cart=" + cart +
+                '}';
     }
 }
