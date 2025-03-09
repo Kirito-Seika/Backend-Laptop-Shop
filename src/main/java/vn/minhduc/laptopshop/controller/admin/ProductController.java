643a82311139c32e99a7016f0aabe585a4f27a93
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-03-09 18:57:15 +0700

    Complete Fetch Product

--- src/main/java/vn/minhduc/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/minhduc/laptopshop/controller/admin/ProductController.java
@@ -32,6 +32,8 @@ public class ProductController {
         Page<Product> prs = this.productService.getAllProducts(pageable);
         List<Product> products = prs.getContent();
         model.addAttribute("listProducts", products);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", prs.getTotalPages());
         return "admin/product/layout";
     }
 
--- src/main/java/vn/minhduc/laptopshop/controller/client/HomeController.java
+++ src/main/java/vn/minhduc/laptopshop/controller/client/HomeController.java
@@ -3,6 +3,9 @@ package vn.minhduc.laptopshop.controller.client;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -41,8 +44,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String getHomePage(Model model) {
-//        List<Product> products = this.productService.getAllProducts();
-//        model.addAttribute("products", products);
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Product> prs = this.productService.getAllProducts(pageable);
+        List<Product> products = prs.getContent();
+        model.addAttribute("products", products);
         return "client/home/layout";
     }
 
