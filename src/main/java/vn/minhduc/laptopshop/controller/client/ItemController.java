244981ea19c7026b187cfebef8bf27d3bd1d6fe8
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-03-09 19:41:48 +0700

    Layout Product

--- src/main/java/vn/minhduc/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/minhduc/laptopshop/controller/client/ItemController.java
@@ -2,6 +2,9 @@ package vn.minhduc.laptopshop.controller.client;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import vn.minhduc.laptopshop.service.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ItemController {
@@ -22,8 +26,29 @@ public class ItemController {
         this.productService = productService;
     }
 
+    @GetMapping("/products")
+    public String getProductPage(Model model, @RequestParam("page") Optional<String> pageOptional) {
+        int page = 1;
+        try {
+            if (pageOptional.isPresent()) {
+                page = Integer.parseInt(pageOptional.get());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        Pageable pageable = PageRequest.of(page - 1, 6);
+        Page<Product> productPage = this.productService.getAllProducts(pageable);
+        List<Product> products = productPage.getContent();
+
+        model.addAttribute("products", products);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", productPage.getTotalPages());
+        return "client/product/layout";
+    }
+
     @GetMapping("/product/{id}")
-    public String getProductPage(Model model, @PathVariable long id) {
+    public String getProductDetailPage(Model model, @PathVariable long id) {
         Product product = this.productService.getProductById(id).get();
         model.addAttribute("product", product);
         model.addAttribute("id", id);
